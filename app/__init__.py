6ddf6d033837c661dd1ae3f94a6ec3d2590de094
heiden.deng <heiden_deng@163.com>
2018-12-19 16:43:50 +0800

    add code

---
+++ app/__init__.py
@@ -0,0 +1,31 @@
+import sys
+sys.path.extend(['/Users/dengjq/dev/python/blog/venv/lib/python3.7/site-packages/'])
+
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap
+from flask_mail import Mail
+from flask_moment import Moment
+from flask_sqlalchemy import SQLAlchemy
+from config import config
+
+bootstrap = Bootstrap()
+mail = Mail()
+moment = Moment()
+db = SQLAlchemy()
+
+
+def create_app(config_name):
+    app = Flask(__name__)
+    app.config.from_object(config[config_name])
+    config[config_name].init_app(app)
+    bootstrap.init_app(app)
+    mail.init_app(app)
+    moment.init_app(app)
+    db.init_app(app)
+    # 附加路由和自定义的错误页面
+
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+    from .auth import auth as auth_blueprint
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
+    return app
