c733270f816be0de8547f06a3a78e5888e1ce7b9
heiden.deng <heiden_deng@163.com>
2018-12-21 16:32:30 +0800

    增加注册用户邮件确认才能激活

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -3,6 +3,7 @@ from wtforms import StringField,PasswordField,BooleanField,SubmitField
 from wtforms.validators import Required,Length,Email,Regexp,EqualTo
 from wtforms import ValidationError
 from ..models import User
+from ..email import send_email
 
 
 class LoginForm(Form):
